f7e3240e60ccfa7af3f0b44dafc52d3c469c37fc
santi-19-hub <santirendon19003@gmail.com>
2026-08-28 22:48:40 -0500

    feat: implementar API Gateway con proxy hacia customers, products y shopping

---
+++ microservices-project-end/gateway/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+
+app.use('/customers', createProxyMiddleware({
+    target: process.env.CUSTOMERS_URL,
+    changeOrigin: true,
+    pathRewrite: { '^/': '/customers/' },
+}));
+
+app.use('/products', createProxyMiddleware({
+    target: process.env.PRODUCTS_URL,
+    changeOrigin: true,
+    pathRewrite: { '^/': '/products/' },
+}));
+
+app.use('/orders', createProxyMiddleware({
+    target: process.env.SHOPPING_URL,
+    changeOrigin: true,
+    pathRewrite: { '^/': '/orders/' },
+}));
+
+
+app.get('/', (req, res) => {
+    res.send('API Gateway is running');
+});
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`🚪 Gateway running on port ${PORT}`);
+});
